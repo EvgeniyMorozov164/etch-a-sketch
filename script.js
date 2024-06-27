d309a3b79690b34163ffc6d592918ec979593f10
Evgeniy Morozov <131901919+EvgeniyMorozov164@users.noreply.github.com>
2024-06-27 19:32:34 +0400

    add basic 16x16 grid

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const container = document.querySelector(".container");
+
+
+const createDiv = () => {
+  const div = document.createElement("div");
+  div.classList.add("grid-item");
+  container.appendChild(div);
+}
+
+const createGrid = () => {
+  const count = 16 ** 2;
+  for (let i = 0; i < count; i++) {
+    createDiv();
+  }
+}
+
+document.addEventListener("load", createGrid());
