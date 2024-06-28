0f0697a1bc4b325da7e0cc2b15fd7b7d3eced09e
Evgeniy Morozov <131901919+EvgeniyMorozov164@users.noreply.github.com>
2024-06-28 19:53:09 +0400

    add user grid render

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 const gridSize = document.querySelector("button");
+const body = document.querySelector("body");
 
 let size = 16;
 
@@ -9,11 +10,9 @@ const getRandomNumber = () => {
 }
 
 const getBackgroundColor = () => {
-  const backgroundColor = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
-  console.log(backgroundColor);
+  const backgroundColor = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;  
   return backgroundColor;
 }
-
 
 const createDiv = () => {  
   const div = document.createElement("div");
@@ -36,6 +35,40 @@ const createGrid = () => {
   }  
 }
 
+const clearGrid = () => {
+  body.removeChild(body.children[1]);
+}
+
+const createUserGrid = () => {
+  const div = document.createElement("div");
+  div.classList.add("container");
+  body.insertBefore(div, document.querySelector("script"));
+  const count = size ** 2;
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement("div");
+    item.classList.add("grid-item");
+    item.style.width = `calc(960px / ${size})`;
+    item.style.height = `calc(960px / ${size})`; 
+    div.appendChild(item);
+    document.addEventListener("mouseover", (e) => {    
+      if (e.target === item) {
+        item.style.backgroundColor = getBackgroundColor();
+      };
+    });
+  };   
+}
+
+gridSize.addEventListener("click", () => {
+  size = prompt("Enter grid size (min: 16; max: 100)", "");
+  if (size < 16) {
+    size = 16;
+  }
+  if (size > 100) {
+    size = 100;
+  }
+  clearGrid();
+  createUserGrid();  
+});
 
 document.addEventListener("load", createGrid());
 
