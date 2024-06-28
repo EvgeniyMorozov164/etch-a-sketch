9fb501e94e4a0968497e8054c923deae6c1e81c5
Evgeniy Morozov <131901919+EvgeniyMorozov164@users.noreply.github.com>
2024-06-28 19:12:05 +0400

    add random color for hover effect

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 const container = document.querySelector(".container");
 const gridSize = document.querySelector("button");
 
+let size = 16;
 
 const getRandomNumber = () => {
   const number = Math.floor(Math.random() * 255);
   return number;
 }
 
-let size = 16;
-const getBackgroundColor = (num) => {
+const getBackgroundColor = () => {
   const backgroundColor = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
   console.log(backgroundColor);
   return backgroundColor;
 }
-const bColor = getBackgroundColor();
+
+
 const createDiv = () => {  
   const div = document.createElement("div");
   div.classList.add("grid-item");
   div.style.width = `calc(960px / ${size})`;
   div.style.height = `calc(960px / ${size})`; 
-  div.style.backgroundColor = bColor;
+  // div.style.backgroundColor = bColor;
   container.appendChild(div);
   document.addEventListener("mouseover", (e) => {    
     if (e.target === div) {
-      div.style.backgroundColor = "red";
-    }
+      div.style.backgroundColor = getBackgroundColor();
+    };
   });
 }
 
